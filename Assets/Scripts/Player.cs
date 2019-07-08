dbc1ef57b883824b84d77df5c0fff6c1fb0df032
232171773@qq.com <667976dsa>
2019-07-09 00:31:20 +0800

    finish play walk

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 4f;
+    public float maxVelocity = 8f;
+
+    Rigidbody2D myBody;
+    Animator anim;
+
+    void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        PlayerMoveKeyboard();
+    }
+
+    void PlayerMoveKeyboard() {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+        float h = Input.GetAxisRaw("Horizontal");
+        if (h > 0) {
+            if (vel < maxVelocity) {
+                forceX = speed;
+                Vector3 temp = transform.localScale;
+                temp.x = 1f;
+                transform.localScale = temp;
+                anim.SetBool("Walk", true);
+            }
+        }
+        else if (h < 0) {
+            if (vel < maxVelocity) {
+                forceX = -speed;
+                Vector3 temp = transform.localScale;
+                temp.x = -1f;
+                transform.localScale = temp;
+                anim.SetBool("Walk", true);
+            }
+        }
+        else {
+            anim.SetBool("Walk", false
+            );
+        }
+
+        myBody.AddForce(new Vector2(forceX, 0));
+    }
+}
