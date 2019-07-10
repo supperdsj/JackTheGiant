65268814e0ac0942fd40760c2d02bd4893f5ad44
232171773@qq.com <667976dsa>
2019-07-10 14:24:59 +0800

    make backgrounds center/scale of screen

---
+++ Assets/Scripts/Background/BGScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScaler : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        Vector3 tempScale = transform.localScale;
+        // Vector3 tempLocation = transform.localPosition;
+
+        float width = sr.sprite.bounds.size.x;
+
+        float worldHeight = Camera.main.orthographicSize * 2.0f;
+        float worldWidth = worldHeight / Screen.height * Screen.width;
+
+        tempScale.x = worldWidth / width;
+        transform.localScale = tempScale;
+
+        // tempLocation.y = 0;
+        // tempLocation.x = -width / 2;
+        // transform.localPosition = tempLocation;
+    }
+}
