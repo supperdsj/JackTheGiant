b431bc4e29a9d5d4e8a6f123d971d63795c571ef
232171773@qq.com <667976dsa>
2019-07-10 19:28:46 +0800

    move camera

---
+++ Assets/Sprites/Camera/CameraScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    float speed = 1f;
+    float acceleration = .2f;
+    float maxSpeed = 3.2f;
+
+    [HideInInspector] public bool moveCamera;
+
+    void Start() {
+        moveCamera = true;
+    }
+
+    void Update() {
+        if (moveCamera) {
+            MoveCamera();
+        }
+    }
+
+    void MoveCamera() {
+        Vector3 temp = transform.position;
+        float oldY = temp.y;
+        float newY = temp.y - (speed * Time.deltaTime);
+        temp.y = Mathf.Clamp(temp.y, oldY, newY);
+        transform.position = temp;
+        speed += acceleration * Time.deltaTime;
+        if (speed > maxSpeed) {
+            speed = maxSpeed;
+        }
+    }
+}
