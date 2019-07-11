1e2dc2958e695f2a13c4382008618c43dcb68b6d
232171773@qq.com <667976dsa>
2019-07-11 15:40:51 +0800

    navgate scenes

---
+++ Assets/Scripts/GameControllers/GameMenuController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuController : MonoBehaviour {
+    public void StartGame() {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HighScoreMenu() {
+        SceneManager.LoadScene("HighScoreMenu");
+    }
+
+    public void OptionsMenu() {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void MusicButton() {
+
+    }
+}
