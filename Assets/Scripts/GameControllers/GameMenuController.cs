ba2a27df9613a3d9038a86f13702a15b8d40cb3a
232171773@qq.com <667976dsa>
2019-07-13 00:53:27 +0800

    enable music controller

--- Assets/Scripts/GameControllers/GameMenuController.cs
+++ Assets/Scripts/GameControllers/GameMenuController.cs
@@ -1,9 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameMenuController : MonoBehaviour {
+    [SerializeField] Button musicButton;
+    [SerializeField] Sprite[] musicIcons;
+
+    void CheckToPlayTheMusic() {
+        if (GamePreferences.GetMusicState() == 1) {
+            MusicController.instanc.PlayMusic(true);
+            musicButton.image.sprite = musicIcons[1];
+        }
+        else {
+            MusicController.instanc.PlayMusic(false);
+            musicButton.image.sprite = musicIcons[0];
+        }
+    }
+
+    void Start() {
+        CheckToPlayTheMusic();
+    }
+
     public void StartGame() {
         GameManagerController.instace.gameStartedFromMainMenu = true;
         GameManagerController.instace.gameRestartedAfterPlayerDied = false;
@@ -23,6 +43,12 @@ public class GameMenuController : MonoBehaviour {
     }
 
     public void MusicButton() {
-
+        if (GamePreferences.GetMusicState() == 0) {
+            GamePreferences.SetMusicState(1);
+        }
+        else {
+            GamePreferences.SetMusicState(0);
+        }
+        CheckToPlayTheMusic();
     }
 }
---
+++ Assets/Scripts/GameControllers/MusicController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+    public static MusicController instanc;
+
+    AudioSource audioSource;
+
+    void Awake() {
+        MakeSingleton();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void PlayMusic(bool play) {
+        if (play) {
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
+        }
+        else {
+            if (audioSource.isPlaying) {
+                audioSource.Stop();
+            }
+        }
+    }
+    void MakeSingleton() {
+        if (instanc != null) {
+            Destroy(this);
+        }
+        else {
+            instanc = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
